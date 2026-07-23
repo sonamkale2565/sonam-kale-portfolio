fe85943c923aa287c1505cec3bfd5eb85bf657f6
Sonam Kale <sonamkale2565@gmail.com>
2026-07-23 14:44:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
+// Typing Animation
 
+const text = [
+    "Web Developer",
+    "AI & Data Science Student",
+    "Video Editor"
+];
+
+let index = 0;
+let charIndex = 0;
+let currentText = "";
+let isDeleting = false;
+
+function typing(){
+
+    currentText = text[index];
+
+    if(!isDeleting){
+
+        document.getElementById("typing").textContent =
+        currentText.substring(0,charIndex++);
+
+        if(charIndex > currentText.length){
+
+            isDeleting = true;
+            setTimeout(typing,1000);
+            return;
+        }
+
+    }
+    else{
+
+        document.getElementById("typing").textContent =
+        currentText.substring(0,charIndex--);
+
+
+        if(charIndex < 0){
+
+            isDeleting = false;
+
+            index++;
+
+            if(index >= text.length){
+                index = 0;
+            }
+
+        }
+
+    }
+
+
+    setTimeout(typing,100);
+
+}
+
+
+typing();
+
+
+
+
+// Current Year
+
+document.getElementById("year").innerHTML =
+new Date().getFullYear();
